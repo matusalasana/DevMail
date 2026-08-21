a58f04c9e96760c47d2c35912572002b27d9c59e
Sana Matusala <matusalasana@gmail.com>
2026-08-21 21:09:01 +0300

    fix: fix axios env import

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: import.meta.env.DEV
+    ? "http://localhost:3000/api/v1"
+    : import.meta.env.VITE_API_URL,
 });
 
 export default api;
